7c5005871c7af96951142ed5babd7642cae8ea61
Daniela Salazar <dsalazara1@eafit.edu.co>
2026-05-01 00:03:30 -0500

    fix compilation errors due to errors in includes

--- modules/HttpServer/src/FileManagerTypes.h
+++ modules/HttpServer/src/FileManagerTypes.h
@@ -2,9 +2,10 @@
 #define FILE_MANAGER_TYPES_H
 
 #define MAX_PATH_LEN 1024
+#include <stddef.h>
 
 // definición real del opaque struct
-struct FileResult {
+typedef struct FileResult {
     // datos del archivo
     char*  _content;           // body a enviar
     size_t _contentLen;        // → Content-Length
@@ -15,6 +16,6 @@ struct FileResult {
     char _location[1024];    // → Location (POST 201, si no, /0)
     // control de errores
     int _statusCode;        // 200, 201, 400, 403, 404, 500
-};
+} FileResult;
 
 #endif
